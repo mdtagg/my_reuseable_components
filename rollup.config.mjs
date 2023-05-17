df8fab630b3f7f5bacbd7550192e352b427211a4
Mike Tagg <mdtagg91@gmail.com>
2023-05-17 12:48:18 -0700

    added postcss

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import packageJson from "./package.json" assert { type: "json" };
+import postcss from "rollup-plugin-postcss";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default [
   {
@@ -15,9 +17,11 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss()
     ],
   },
   {
